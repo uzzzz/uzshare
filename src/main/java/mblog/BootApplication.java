190ccd877e169b2365d039717354beac791f2eca
fengjianglin <fengjianglin@163.com>
2018-08-09 12:26:38 +0800

    error pages

--- src/main/java/mblog/BootApplication.java
+++ src/main/java/mblog/BootApplication.java
@@ -5,25 +5,41 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 /**
  * SprintBootApplication
  */
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
 public class BootApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BootApplication.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootApplication.class);
+	}
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				ErrorPage e404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html");
+				container.addErrorPages(e404);
+			}
+		};
+	}
 
 }
