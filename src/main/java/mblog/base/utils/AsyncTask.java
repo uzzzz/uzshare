d1a798aae95862afba1f1a75f7a6488fa4af3657
fengjianglin <fengjianglin@163.com>
2018-08-16 12:02:56 +0800

    post

--- src/main/java/mblog/base/utils/AsyncTask.java
+++ src/main/java/mblog/base/utils/AsyncTask.java
@@ -1,10 +1,13 @@
 package mblog.base.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,21 +19,29 @@ public class AsyncTask {
 	@Autowired
 	private RestTemplate rest;
 
+	@Bean
+	public TaskExecutor getTaskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setMaxPoolSize(10);
+		taskExecutor.setCorePoolSize(5);
+		taskExecutor.setQueueCapacity(20);
+		return taskExecutor;
+	}
+
 	@Async
 	public void postBaiduForBlog(long id) {
-		try {
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.TEXT_PLAIN);
-			String content = "https://blog.uzzz.org/view/" + id;
-			HttpEntity<String> requestEntity = new HttpEntity<String>(content, headers);
-			// 执行HTTP请求
-			String ret = rest.postForObject(
-					"http://data.zz.baidu.com/urls?site=https://blog.uzzz.org&token=pJ67TFnK02hkMHlt", requestEntity,
-					String.class);
-			Printer.info("post baidu : " + ret + " (" + content + ")");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			String postUrl = "http://data.zz.baidu.com/urls?site=https://blog.uzzz.org&token=pJ67TFnK02hkMHlt";
+//			HttpHeaders headers = new HttpHeaders();
+//			headers.setContentType(MediaType.TEXT_PLAIN);
+//			String content = "https://blog.uzzz.org/view/" + id;
+//			HttpEntity<String> requestEntity = new HttpEntity<String>(content, headers);
+//			// 执行HTTP请求
+//			String ret = rest.postForObject(postUrl, requestEntity, String.class);
+//			Printer.info("post baidu : " + ret + " (" + content + ")");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
