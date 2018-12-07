3811aa0a5e38a2bce2c39d4f3ed3d5fa4bab609c
fengjianglin <fjl@lenovo>
2018-12-07 10:51:17 +0800

    unit test

---
+++ src/test/java/mblog/EmailTest.java
@@ -0,0 +1,36 @@
+package mblog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import mblog.base.lang.Consts;
+import mblog.base.print.Printer;
+import mblog.base.utils.MailHelper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BootApplication.class)
+public class EmailTest {
+
+	@Autowired
+	private MailHelper mailHelper;
+
+	@Test
+	public void testSendMall() {
+
+		Map<String, Object> context = new HashMap<>();
+		context.put("userId", "1");
+		context.put("code", "123456");
+		context.put("type", Consts.VERIFY_BIND);
+		String email = "fandyvon@163.com";
+		Printer.error("sending email start");
+		mailHelper.sendEmail(Consts.EMAIL_TEMPLATE_BIND, email, "邮箱绑定验证", context);
+		Printer.error(email + " send success");
+	}
+
+}
