7d0b681e03fd754160cfb81b53dc3eb3864b4bf5
fengjianglin <fjl@lenovo>
2019-01-30 10:48:13 +0800

    ehcache 3.x listener

---
+++ src/main/java/uzblog/boot/CacheEventLogger.java
@@ -0,0 +1,18 @@
+package uzblog.boot;
+
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CacheEventLogger.class);
+
+	@Override
+	public void onEvent(CacheEvent<? extends Object, ? extends Object> event) {
+		LOGGER.info("Event: " + event.getType() + " Key: " + event.getKey() + " old value: " + event.getOldValue()
+				+ " new value: " + event.getNewValue());
+
+	}
+}
