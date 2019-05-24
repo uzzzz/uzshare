59f61128b777bf4986d35c6c81ab7cc410153ee6
fengjianglin <fjl@lenovo>
2019-05-24 10:51:59 +0800

    login代码重构

--- src/main/java/uzblog/web/controller/site/auth/LoginController.java
+++ src/main/java/uzblog/web/controller/site/auth/LoginController.java
@@ -11,15 +11,17 @@ package uzblog.web.controller.site.auth;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import uzblog.modules.user.service.NotifyService;
 import uzblog.web.controller.BaseController;
 import uzblog.web.controller.site.Views;
 
@@ -29,58 +31,52 @@ import uzblog.web.controller.site.Views;
  */
 @Controller
 public class LoginController extends BaseController {
-    @Autowired
-    private NotifyService notifyService;
 
-    /**
-     * 跳转登录页
-     * @return
-     */
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String view() {
+	@GetMapping(value = "login")
+	public String login() {
 		return view(Views.LOGIN);
 	}
 
-    /**
-     * 提交登录
-     * @param username
-     * @param password
-     * @param model
-     * @return
-     */
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(String username, String password,@RequestParam(value = "rememberMe",defaultValue = "0") int rememberMe, ModelMap model) {
+	/**
+	 * 提交登录
+	 * 
+	 * @param username
+	 * @param password
+	 * @param model
+	 * @return
+	 */
+	@PostMapping(value = "login")
+	public String do_login(String username, String password,
+			@RequestParam(value = "rememberMe", defaultValue = "0") int rememberMe, ModelMap model) {
 		String ret = view(Views.LOGIN);
-		
+
 		if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
-            return ret;
-        }
-		
+			return ret;
+		}
+
 		AuthenticationToken token = createToken(username, password);
-        if (token == null) {
-        	model.put("message", "用户名或密码错误");
-            return ret;
-        }
+		if (token == null) {
+			model.put("message", "用户名或密码错误");
+			return ret;
+		}
 
-        if (rememberMe == 1) {
-            ((UsernamePasswordToken) token).setRememberMe(true);
-        }
+		if (rememberMe == 1) {
+			((UsernamePasswordToken) token).setRememberMe(true);
+		}
 
-        try {
-            SecurityUtils.getSubject().login(token);
+		try {
+			SecurityUtils.getSubject().login(token);
 
-            ret = Views.REDIRECT_USER;
-        } catch (AuthenticationException e) {
-            if (e instanceof UnknownAccountException) {
-            	model.put("message", "用户不存在");
-            } else if (e instanceof LockedAccountException) {
-            	model.put("message", "用户被禁用");
-            } else {
-            	model.put("message", "用户认证失败");
-            }
-        }
-
-        return ret;
+			ret = Views.REDIRECT_USER;
+		} catch (AuthenticationException e) {
+			if (e instanceof UnknownAccountException) {
+				model.put("message", "用户不存在");
+			} else if (e instanceof LockedAccountException) {
+				model.put("message", "用户被禁用");
+			} else {
+				model.put("message", "用户认证失败");
+			}
+		}
+		return ret;
 	}
-
 }
