833bb7eb9de3a52f8c2b99247a242a73a0e27ab6
fengjianglin <fjl@lenovo>
2019-08-05 09:57:24 +0800

    添加角色BUG

--- src/main/java/uzblog/modules/authc/service/impl/RoleServiceImpl.java
+++ src/main/java/uzblog/modules/authc/service/impl/RoleServiceImpl.java
@@ -1,5 +1,13 @@
 package uzblog.modules.authc.service.impl;
 
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.criteria.Predicate;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,7 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import uzblog.modules.authc.dao.PermissionDao;
 import uzblog.modules.authc.dao.RoleDao;
 import uzblog.modules.authc.dao.UserRoleDao;
 import uzblog.modules.authc.entity.Permission;
@@ -18,117 +25,110 @@ import uzblog.modules.authc.entity.UserRole;
 import uzblog.modules.authc.service.RolePermissionService;
 import uzblog.modules.authc.service.RoleService;
 
-import javax.persistence.criteria.Predicate;
-import java.util.*;
-
-/**
- * @author - langhsu on 2018/2/11
- */
 @Service
 @Transactional
 public class RoleServiceImpl implements RoleService {
-    @Autowired
-    private RoleDao roleDao;
-    @Autowired
-    private PermissionDao permissionDao;
-    @Autowired
-    private RolePermissionService rolePermissionService;
-    @Autowired
-    private UserRoleDao userRoleDao;
 
-    @Override
-    public Page<Role> paging(Pageable pageable, String name) {
-        Page<Role> page = roleDao.findAll((root, query, builder) -> {
-            Predicate predicate = builder.conjunction();
+	@Autowired
+	private RoleDao roleDao;
+	@Autowired
+	private RolePermissionService rolePermissionService;
+	@Autowired
+	private UserRoleDao userRoleDao;
 
-            if (StringUtils.isNoneBlank(name)) {
-                predicate.getExpressions().add(
-                        builder.like(root.get("name"), "%" + name + "%"));
-            }
+	@Override
+	public Page<Role> paging(Pageable pageable, String name) {
+		Page<Role> page = roleDao.findAll((root, query, builder) -> {
+			Predicate predicate = builder.conjunction();
 
-            query.orderBy(builder.desc(root.get("id")));
-            return predicate;
-        }, pageable);
-        return page;
-    }
+			if (StringUtils.isNoneBlank(name)) {
+				predicate.getExpressions().add(builder.like(root.get("name"), "%" + name + "%"));
+			}
 
-    @Override
-    public List<Role> list() {
-        List<Role> list = roleDao.findAllByStatusOrderByIdDesc(Role.STATUS_NORMAL);
-        return list;
-    }
+			query.orderBy(builder.desc(root.get("id")));
+			return predicate;
+		}, pageable);
+		return page;
+	}
 
-    @Override
-    public Map<Long, Role> findByIds(Set<Long> ids) {
-        List<Role> list = roleDao.findAllByIdIsIn(ids);
-        Map<Long, Role> ret = new LinkedHashMap<>();
-        list.forEach(po -> {
-            Role vo = toVO(po);
-            ret.put(vo.getId(), vo);
-        });
-        return ret;
-    }
+	@Override
+	public List<Role> list() {
+		List<Role> list = roleDao.findAllByStatusOrderByIdDesc(Role.STATUS_NORMAL);
+		return list;
+	}
 
-    @Override
-    public Role get(long id) {
-        Role po = roleDao.getOne(id);
-        return toVO(po);
-    }
+	@Override
+	public Map<Long, Role> findByIds(Set<Long> ids) {
+		List<Role> list = roleDao.findAllByIdIsIn(ids);
+		Map<Long, Role> ret = new LinkedHashMap<>();
+		list.forEach(po -> {
+			Role vo = toVO(po);
+			ret.put(vo.getId(), vo);
+		});
+		return ret;
+	}
 
-    @Override
-    public void update(Role r, Set<Permission> permissions) {
-        Role po = roleDao.getOne(r.getId());
+	@Override
+	public Role get(long id) {
+		Role po = roleDao.getOne(id);
+		return toVO(po);
+	}
 
-        if (null == po) {
-            po = new Role();
-            po.setName(r.getName());
-        }
-        po.setDescription(r.getDescription());
-        po.setStatus(r.getStatus());
+	@Override
+	public void update(Role r, Set<Permission> permissions) {
+		Role po = roleDao.existsById(r.getId()) ? //
+				roleDao.getOne(r.getId()) : null;
 
-        roleDao.save(po);
+		if (null == po) {
+			po = new Role();
+			po.setName(r.getName());
+		}
+		po.setDescription(r.getDescription());
+		po.setStatus(r.getStatus());
 
-        rolePermissionService.deleteByRoleId(po.getId());
+		roleDao.save(po);
 
-        if (permissions != null && permissions.size() > 0) {
-            Set<RolePermission> rps = new HashSet<>();
-            long roleId = po.getId();
-            permissions.forEach(p -> {
-                RolePermission rp = new RolePermission();
-                rp.setRoleId(roleId);
-                rp.setPermissionId(p.getId());
-                rps.add(rp);
-            });
+		rolePermissionService.deleteByRoleId(po.getId());
 
-            rolePermissionService.add(rps);
-        }
-    }
+		if (permissions != null && permissions.size() > 0) {
+			Set<RolePermission> rps = new HashSet<>();
+			long roleId = po.getId();
+			permissions.forEach(p -> {
+				RolePermission rp = new RolePermission();
+				rp.setRoleId(roleId);
+				rp.setPermissionId(p.getId());
+				rps.add(rp);
+			});
 
-    @Override
-    public boolean delete(long id) {
-        List<UserRole> urs = userRoleDao.findAllByRoleId(id);
-        Assert.state(urs == null || urs.size() == 0, "该角色已经被使用,不能被删除");
-        roleDao.deleteById(id);
-        rolePermissionService.deleteByRoleId(id);
-        return true;
-    }
+			rolePermissionService.add(rps);
+		}
+	}
 
-    @Override
-    public void activate(long id, boolean active) {
-        Role po = roleDao.getOne(id);
-        if (po != null) {
-            po.setStatus(active ? Role.STATUS_NORMAL : Role.STATUS_CLOSED);
-        }
-    }
+	@Override
+	public boolean delete(long id) {
+		List<UserRole> urs = userRoleDao.findAllByRoleId(id);
+		Assert.state(urs == null || urs.size() == 0, "该角色已经被使用,不能被删除");
+		roleDao.deleteById(id);
+		rolePermissionService.deleteByRoleId(id);
+		return true;
+	}
 
-    private Role toVO(Role po) {
-        Role r = new Role();
-        r.setId(po.getId());
-        r.setName(po.getName());
-        r.setDescription(po.getDescription());
-        r.setStatus(po.getStatus());
+	@Override
+	public void activate(long id, boolean active) {
+		Role po = roleDao.getOne(id);
+		if (po != null) {
+			po.setStatus(active ? Role.STATUS_NORMAL : Role.STATUS_CLOSED);
+		}
+	}
 
-        r.setPermissions(rolePermissionService.findPermissions(r.getId()));
-        return r;
-    }
+	private Role toVO(Role po) {
+		Role r = new Role();
+		r.setId(po.getId());
+		r.setName(po.getName());
+		r.setDescription(po.getDescription());
+		r.setStatus(po.getStatus());
+
+		r.setPermissions(rolePermissionService.findPermissions(r.getId()));
+		return r;
+	}
 }
